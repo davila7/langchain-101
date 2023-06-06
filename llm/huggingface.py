e7b5ff1e6db4db4358214a129a3d72f5bff75452
Dani <dan.avila7@gmail.com>
2023-06-05 20:26:07 -0400

    fix huggingface

--- llm/huggingface.py
+++ llm/huggingface.py
@@ -1,29 +1,23 @@
-from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 from dotenv import load_dotenv
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
 import os
 
 """
 HuggingFace falcon
 
 """
-
-
 # cargamos apikey
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-template = '''
-Question: {question}
-Answer: Let's think step by step.
-'''
+template = """Question: {question}
 
-prompt = PromptTemplate(template=template, input_variables=['question'])
+Answer: Let's think step by step."""
+prompt = PromptTemplate(template=template, input_variables=["question"])
 llm = HuggingFaceHub(repo_id='tiiuae/falcon-7b-instruct', model_kwargs={"temperature":0.3})
-
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-question = "Who is your creator?"
+question = "Who won the FIFA World Cup in the year 1998? "
 print(question)
 print('Falcon: ', llm_chain.run(question))
 
