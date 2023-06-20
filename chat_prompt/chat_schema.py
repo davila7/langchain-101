09e23653378a219e8ffb4b3721b52c2b610e2011
Dani <dan.avila7@gmail.com>
2023-06-20 16:53:14 -0400

    chat models

---
+++ chat_prompt/chat_schema.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chat_models import ChatOpenAI
+
+
+load_dotenv()
+chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.1)
+messages = [
+    SystemMessage(content="Eres un experto en la historia del futbol"),
+    HumanMessage(content="Quién ganó la copa del mundo de Francia 98?")
+]
+response = chat(messages)
+
+print(response)
+
