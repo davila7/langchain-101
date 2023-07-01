60f540147c984b7f65da51093fa94ae99331af7a
Dani <dan.avila7@gmail.com>
2023-06-30 23:50:37 -0400

    vertex

--- llm/llm_vertexai.py
+++ llm/llm_vertexai.py
@@ -4,13 +4,11 @@ import os
 
 """
 VertexAI
-
 """
 
-# cargamos apikey
 load_dotenv()
 llm = VertexAI()
 
-# entregamos la variable text como variable al modelo
-text = "Hola cómo estás?"
+
+text = "Hi"
 print(llm(text))
